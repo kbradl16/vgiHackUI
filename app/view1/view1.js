4734aba16268e40cf26fe2f1a4fdd2a4ec209354
kbradl16 <kbradl16@gmail.com>
2017-07-27 11:39:38 -0400

    controller as

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,11 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+		controllerAs: 'view1'
   });
 }])
 
 .controller('View1Ctrl', [function() {
-
+	this.test = "Test";
 }]);
